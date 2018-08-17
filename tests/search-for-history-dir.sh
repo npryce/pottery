519ba3adbdf3ecaa92a6672fb99d9d0d32cd491c
Nat Pryce <sw@natpryce.com>
2018-08-17 19:16:10 +0100

    a supporting script to help with cross platform behaviour

--- tests/search-for-history-dir.sh
+++ tests/search-for-history-dir.sh
@@ -3,7 +3,6 @@ pottery post First Record
 mkdir subdir
 cd subdir
 pottery post Second Record
-ls docs/project-history
+file-exists docs/project-history
 cd ..
 find docs/project-history -print | sort
-
