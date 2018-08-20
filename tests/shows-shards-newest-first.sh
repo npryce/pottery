fefab1e86155ff2df0ff04da804c86e28f4f8a99
Nat Pryce <sw@natpryce.com>
2018-08-20 17:38:33 +0100

    A pottery show command to read recent shards

---
+++ tests/shows-shards-newest-first.sh
@@ -0,0 +1,5 @@
+pottery init
+TEST_TIME=1992-01-12T16:34:23Z pottery post "First post"
+TEST_TIME=1992-01-12T17:00:00Z pottery post "Second post."
+TEST_TIME=1992-01-13T16:34:23Z pottery post "It's a new day."
+pottery show
